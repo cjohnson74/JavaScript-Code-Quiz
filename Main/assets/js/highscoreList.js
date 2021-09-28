7331087205fc6344d96f768365284212ffc1724d
Carson Johnson <carson74johnson@gmail.com>
2021-09-28 01:52:09 -0400

    added all structural JavaScript and HTML

--- Main/assets/js/highscoreList.js
+++ Main/assets/js/highscoreList.js
@@ -1,18 +1,25 @@
-var highscoreList = document.querySelector("#highscore-listff");
+var highscoreList = document.querySelector("#highscore-list");
 var clearHsBtn = document.querySelector("#clear-highscores");
-var savedScores = localStorage.getItem("highscore");
-
+var getScores = localStorage.getItem("highscore");
+var savedScores = [...JSON.parse(getScores)]
 
 function clearHighscores() {
   window.localStorage.clear();
 }
 
-for (var i = 0; i < savedScores.length - 1; i++){
-    for (var j = 0; i < 2; i++){
-        var row
-    }
+for (var i = 0; i < savedScores.length; i++) {
+    var prevTr = document.createElement("tr");
+    highscoreList.appendChild(prevTr);
+    var tD = document.createElement("td");
+    tD.setAttribute("class", "scoreRow");
+    var tD2 = document.createElement("td");
+    tD2.setAttribute("class", "scoreRow");
+    tD.textContent = savedScores[i].name;
+    tD2.textContent = savedScores[i].score;
+    prevTr.appendChild(tD2);
+    prevTr.appendChild(tD);
+  
 }
-
 
 // for (var i = 1; i < localStorage.length; i++) {
 //   for (var j = 0; j < 9; j++) {
